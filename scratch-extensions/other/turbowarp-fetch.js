a3aae42e8c9ee7048b2f833679a622e113cc4742
Tiger1228cym <102411126+Tiger1228cym@users.noreply.github.com>
2022-10-07 19:33:48 +0800

    index.html

---
+++ scratch-extensions/other/turbowarp-fetch.js
@@ -0,0 +1,30 @@
+// This extension is from https://extensions.turbowarp.org/fetch.js , Adapted by Tiger
+class Fetch_Chinese {
+    getInfo() {
+        return {
+            id: 'fetch',
+            name: 'Fetch',
+            blocks: [
+                {
+                    opcode: 'get',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: '获取 [URL] 的内容',
+                    arguments: {
+                        URL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'https://extensions.turbowarp.org/hello.txt'
+                        }
+                    }
+                }
+            ]
+        };
+    }
+
+    get(args) {
+        return fetch(args.URL)
+            .then(r => r.text())
+            .catch(() => '');
+    }
+}
+
+Scratch.extensions.register(new Fetch_Chinese());
